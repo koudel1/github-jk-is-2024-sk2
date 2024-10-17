83ffa653e3c136c38ef60e8e2368ce8a88f1183e
Jakub Kumšta <jakub.kumsta@tul.cz>
2024-10-17 14:12:33 +0200

    vsechno

---
+++ program002a-soucet-cifer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System; 
+
+class   program{
+
+}
+static void main();
+
+
+
+
+// chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("*************************************");
+            Console.WriteLine("******* Součet a součin cifer *******");
+            Console.WriteLine("*************************************");
+            Console.WriteLine("*********** Jakub Kumšta *************");
+            Console.WriteLine("*************************************\n\n");
+            Console.WriteLine();
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte celé číslo: ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte celé číslo: ");
+            }
+            console.WriteLine("****************************");
+            console.WriteLine("uživatel zadal: {0}",number);
+            console.WriteLine("****************************");
+
+//pokud je číslo záporné, tak - odstraníme
+if(number <0 ){
+    number = - number;
+}
+
+int suma = 0;
+int multi = 1
+int numberbackup = number;
+int digit;
+
+while (numberbackup >=10) {
+   digit = number % 10; // % operátor modulo
+   number = (number - digit) / 10;
+   console.WriteLine("Digit = {0}", digit);
+   suma = suma +digit;
+   multi = multi * digit;
+};
+console.WriteLine('Digit = {0}',number)
+suma = suma + number;
+multi = multi * number
+
+console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberbackup, suma);
+console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberbackup, multi);
+
+//opakování programu
+console.WriteLine("pro opakování programu stiskněte klávesu a");
+again = console.ReadLine();
+}
---
+++ Úkol 003 - vykreslování obrazců #3/Program.cs
@@ -0,0 +1,53 @@
+﻿// chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("*************************************");
+            Console.WriteLine("******* obdelník *******");
+            Console.WriteLine("*************************************");
+            Console.WriteLine("*********** Jakub Kumšta *************");
+            Console.WriteLine("*************************************\n\n");
+            Console.WriteLine();
+            // Vstup od uživatele - lepší varianta
+            Console.Write("Zadejte šířku obrazce (celé číslo): ");
+            int width;
+            while(!int.TryParse(Console.ReadLine(), out  width)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu šířku obrazce (celé číslo): ");
+            }
+
+            Console.Write("Zadejte šířku obrazce (celé číslo): ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out  height)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte znovu výšku obrazce (celé číslo): ");
+}
+
+
+
+
+//for (int i = 1;i<=10; i++){
+//Console.WriteLine(i);
+//}
+
+//int j = 1;
+//while(j <= 10){
+   // Console.WriteLine(j);
+    //j++;
+//}
+
+
+for(int i= 1; i<= height; i++) {
+    for(int j= 1 ;j<= width; j++){
+        Console.Write("* ");
+        System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+            //opakování programu
+Console.WriteLine("pro opakování programu stiskněte klávesu a");
+again = Console.ReadLine();
+}
